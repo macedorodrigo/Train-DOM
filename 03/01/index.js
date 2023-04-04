0ca46b4e9029c5a12611f6c3da639c99fd46c50a
macedorodrigo <romaco1694@gmail.com>
2023-04-04 17:14:43 -0300

    Train events

---
+++ 03/01/index.js
@@ -0,0 +1,31 @@
+const title = document.createElement('h1')
+title.innerText = 'Começando teste'
+document.body.appendChild(title)
+
+const main = document.createElement('main')
+main.id = 'main'
+document.body.appendChild(main)
+
+const p = document.createElement('p')
+p.id = 'paragraph'
+main.appendChild(p)
+
+const maisInfos = () => {
+  const a = document.getElementById('paragraph')
+  a.innerText = 'Text here'
+}
+
+const button = document.createElement('button')
+button.innerText = 'Click here!!!!'
+button.name = 'Click here!!!'
+button.value = 'Click here'
+button.onclick = maisInfos;
+main.appendChild(button)
+
+
+windown.onload = newCall
+
+const newCall = () => {
+  alert('pizza pronta')
+  alert('Pizza à caminho')
+}
